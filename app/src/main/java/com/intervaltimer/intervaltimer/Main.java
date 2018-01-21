d0dabb11b2cdfe08653056a2f8c8128afe5fb834
BeccyGitHub <34550836+BeccyGitHub@users.noreply.github.com>
2018-01-21 10:40:50 +0100

    Initial commit

---
+++ app/src/main/java/com/intervaltimer/intervaltimer/Main.java
@@ -0,0 +1,19 @@
+package com.intervaltimer.intervaltimer;
+
+import android.os.Bundle;
+
+/**
+ * Created by Rebecca on 02.01.2018. This is an Intervaltimer.
+ */
+
+public class Main {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
